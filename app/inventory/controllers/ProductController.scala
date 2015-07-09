ad366b9939d0b052c7e4bf47f41698d3c5b577da
Jun Yamog <jkyamog@gmail.com>
2015-07-09 22:08:56 +1200

    add slick and postgres

--- app/inventory/controllers/ProductController.scala
+++ app/inventory/controllers/ProductController.scala
@@ -4,11 +4,18 @@ import inventory.domain.Product.ProductAggregate
 import inventory.events.CreateProduct
 import inventory.storage.EventStore
 import inventory.domain.{AggregateRoot, Product}
+import play.api.Play
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfig}
 
 import play.api.libs.json._
 import play.api.mvc._
+import slick.driver.JdbcProfile
 
-class ProductController extends Controller {
+
+class ProductController extends Controller with HasDatabaseConfig[JdbcProfile] {
+  val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
+
+  import dbConfig.driver.api._
 
   implicit val productFormatter = Json.format[Product]
   implicit val productCreateFormatter = Json.format[CreateProduct]
