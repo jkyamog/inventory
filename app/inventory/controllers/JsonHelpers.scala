97a85de562a8d18374bcca176af45a25e3b815c4
Jun Yamog <jkyamog@gmail.com>
2015-08-08 23:04:16 +1200

    Some consolidation.  Make use of constant strings, still not the best.  Some way to use the type and format macro can be done.  Or just use combinators straight away.

--- app/inventory/controllers/JsonHelpers.scala
+++ app/inventory/controllers/JsonHelpers.scala
@@ -1,13 +1,19 @@
 package inventory.controllers
 
-import inventory.commands.{SellItem, CreateItem}
+import inventory.commands._
 import inventory.domain.Item
+import inventory.events._
 import play.api.libs.json.Json
 
 object JsonHelpers {
 
   implicit val itemFormatter = Json.format[Item]
-  implicit val itemCreateFormatter = Json.format[CreateItem]
+
+  implicit val createItemFormatter = Json.format[CreateItem]
   implicit val sellItemFormatter = Json.format[SellItem]
+
+  implicit val itemCreatedFormatter = Json.format[ItemCreated]
+  implicit val itemSoldFormatter = Json.format[ItemSold]
+  implicit val sellFailedNotificationFormatter = Json.format[SellFailedNotification]
 
 }
--- app/inventory/storage/JsonFormatter.scala
+++ app/inventory/storage/JsonFormatter.scala
@@ -5,23 +5,24 @@ import play.api.libs.json.{Json, JsResult, JsValue, Format}
 
 object JsonFormatter {
 
+  import inventory.controllers.JsonHelpers._
+  val ITEM_CREATED = ItemCreated.toString
+  val ITEM_SOLD = ItemSold.toString
+  val SELL_FAILED_NOTIFICATION = SellFailedNotification.toString
+
   implicit val eventFormatter = new Format[Event] {
-    def reads(js: JsValue): JsResult[Event] = { // TODO refactor remove boiler plate
+    def reads(js: JsValue): JsResult[Event] = {
       val eventType = (js \ "type").as[String]
       eventType match {
-        case "ItemCreated" =>
-          implicit val productCreateFormatter = Json.format[ItemCreated]
-
+        case ITEM_CREATED =>
           val event = (js \ "event").get
           Json.fromJson[ItemCreated](event)
-        case "ItemSold" =>
-          implicit val sellProductFormatter = Json.format[ItemSold]
 
+        case ITEM_SOLD =>
           val event = (js \ "event").get
           Json.fromJson[ItemSold](event)
-        case "SellFailedNotification" =>
-          implicit val formatter = Json.format[SellFailedNotification]
 
+        case SELL_FAILED_NOTIFICATION =>
           val event = (js \ "event").get
           Json.fromJson[SellFailedNotification](event)
 
@@ -32,23 +33,16 @@ object JsonFormatter {
     def writes(event: Event): JsValue = {
       event match {
         case c: ItemCreated =>
-          implicit val productCreateFormatter = Json.format[ItemCreated]
-
           val json = Json.toJson(c)
+          Json.obj("type" -> ITEM_CREATED, "event" -> json)
 
-          Json.obj("type" -> "ItemCreated", "event" -> json)
         case c: ItemSold =>
-          implicit val sellProductFormatter = Json.format[ItemSold]
-
           val json = Json.toJson(c)
+          Json.obj("type" -> ITEM_SOLD, "event" -> json)
 
-          Json.obj("type" -> "ItemSold", "event" -> json)
         case c: SellFailedNotification =>
-          implicit val formatter = Json.format[SellFailedNotification]
-
           val json = Json.toJson(c)
-
-          Json.obj("type" -> "SellFailedNotification", "event" -> json)
+          Json.obj("type" -> SELL_FAILED_NOTIFICATION, "event" -> json)
 
       }
     }
